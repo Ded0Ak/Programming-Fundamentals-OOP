23c7c20188447b703d97ad7c42ab3ed053c1e934
DedAk <davidkagricenok@gmail.com>
2025-03-06 02:38:49 +0200

    q

--- Labs/Lab 2/TS/MyStrManip.js
+++ Labs/Lab 2/TS/MyStrManip.js
@@ -1,22 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.StringManipulator = void 0;
-// StringManipulator.ts
 var StringManipulator = /** @class */ (function () {
-    // Constructor
     function StringManipulator(value) {
         if (value === void 0) { value = ""; }
         this.value = value;
     }
-    // Method to get the string value
     StringManipulator.prototype.getValue = function () {
         return this.value;
     };
-    // Method to get the string length
     StringManipulator.prototype.getLength = function () {
         return this.value.length;
     };
-    // Method to sort the string in descending order
     StringManipulator.prototype.sortDescending = function () {
         return this.value.split("").sort().reverse().join("");
     };
